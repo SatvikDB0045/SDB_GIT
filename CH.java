3a22da55551046f3dd77a1e18e681363dd2602a0
SatvikDB0045 <vvce23ise0045@vvce.ac.in>
2026-03-02 10:21:59 +0530

    Update CH.java
    Print statement added

--- CH.java
+++ CH.java
@@ -1,6 +1,7 @@
 import java.util.*;
 class CH{
     public static void main(String args[]){
-    System..println("Hello World!");
+    System.out.println("Hello World!");
+        System.out.println("JAVA PROGRAMING");
         }
 }
