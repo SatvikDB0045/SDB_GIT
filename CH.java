6c22a5a344b64e0ec221f216186a8397b8cd2324
SatvikDB0045 <vvce23ise0045@vvce.ac.in>
2026-03-09 10:39:56 +0530

    Update CH.java

--- CH.java
+++ CH.java
@@ -2,7 +2,7 @@ import java.util.*;
 class CH{
     public static void main(String args[]){
     System.out.println("Hello World!");
-        System.out.println("JAVA PROGRAMING");
+        System.out.println("JAVA PROGRAM");
         System.out.println("-------");
          System.out.println("--45-----");
         System.out.println("--45----435-");
