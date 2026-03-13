4af01c209775974902ba6e19429528372725b7ae
SatvikDB0045 <vvce23ise0045@vvce.ac.in>
2026-03-13 13:08:37 +0530

    Update CH.java

--- CH.java
+++ CH.java
@@ -3,17 +3,7 @@ class CH{
     public static void main(String args[]){
     System.out.println("Hello World!");
         System.out.println("JAVA PROGRAMer");
-        System.out.println("-------");
-         System.out.println("--45-----");
-        System.out.println("--45----435-");
-        System.out.println("--45--34--435-");
-        System.out.println("--45----43345-");
-          System.out.println("--45----445-");
-        System.out.println("--45----445-");
-        System.out.println("--45----445-");
-        System.out.println("--45----445-");
-         System.out.println("--45----445-");
-         System.out.println("--45----445-");
+      
         
         }
 }
